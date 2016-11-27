a2d178833ab64d950d22d3ccaac39b00279af016
Henrique Thadeu <henriquetsc@MacBook-Pro.local>
2016-11-27 08:52:39 -0200

    Merge

--- platforms/android/assets/www/assets/js/opcoes.js
+++ platforms/android/assets/www/assets/js/opcoes.js
@@ -12,7 +12,7 @@ function scan() {
     },
     {
         "preferFrontCamera" : false, // iOS and Android
-        "showFlipCameraButton" : true, // iOS and Android
+        "showFlipCameraButton" : false, // iOS and Android
         "prompt" : "Place a barcode inside the scan area", // supported on Android only */
        /* "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED */
         "orientation" : "landscape" // Android only (portrait|landscape), default unset so it rotates with the device
@@ -23,6 +23,13 @@ function scan() {
 
  function openScanner(event) {
      event.preventDefault();
-     scan();
-     document.getElementById("spanValor").innerHTML = "R$" + Math.floor(Math.random() * 49 + 11) + ",00";
+     var data = scan();
+
+     if (!data.cancelled) {
+         $("#spanValor").text("R$37,00");
+         $("#divValor").show();
+     } else {
+         $("#spanValor").text("Seleção cancelada");
+         $("#divValor").hide();
+     }
  }
